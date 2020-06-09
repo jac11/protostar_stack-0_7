fcfc36622a95380af61cd3f7e927626b773e19a9
jac11 <49693666+jac11@users.noreply.github.com>
2020-06-09 13:36:32 -0400

    Update script.py

--- script.py
+++ script.py
@@ -4,22 +4,23 @@ import string
 import random
 import argparse
 import sys
+import os 
 
 class Tools :
    
     def __init__(self):
 
-	    parser = argparse.ArgumentParser( description="Usage: [OPtion] [arguments]  [length]  [arguments] Example: -c a -l 300")
-	    parser.add_argument( '-c',"--char"     , metavar='' , action=None  , help ="to generate A or B..etc char " "Example : -c a -l 220")
-	    parser.add_argument( '-r',"--random"   , metavar='' , action =None , help ="to gentreta random pattern " "Example : -r r -l 200", type = str)
-	    parser.add_argument( '-a',"--alfa"     , metavar='' , action =None , help ="to generate AAAA to ZZZZ 32bit " "Example : -a x86 -l 26 ")
-	    parser.add_argument( '-A',"--alfa_64"  , metavar='' , action =None, help ="generate AAAAAAAA to ZZZZZZZZ 64bit " "Example :-A x64 -l 26")
-	    parser.add_argument( '-H',"--Hexdecode", metavar='' , action =None , help ="to decode hex address ""Exampel: -H 0x00000000", type =str)
-	    parser.add_argument( '-l',"--length"   , metavar='' , action =None , help ="to give length of the pattern ", type = int)
-	    parser.add_argument( '-s',"--L_endian", metavar='' , action =None , help ="to little-endian struct " "Exsampel: -s 0x0876bf67") 
-	    self.args = parser.parse_args()
-	    self.Main()    
-       
+	parser = argparse.ArgumentParser( description="Usage: [OPtion] [arguments]  [length]  [arguments] Exsample: -c a -l 300")
+	parser.add_argument( '-c',"--char"     , metavar='' , action=None  ,help ="to generate A or B..etc char " "Exsample : -c a -l 220")
+	parser.add_argument( '-r',"--random"   , metavar='' , action =None ,help ="to gentreta random pattern " "Exsample : -r r -l 200", type = str)
+	parser.add_argument( '-a',"--alfa"     , metavar='' , action =None ,help ="to generate AAAAtoZZZZ 32bit " "Exsample : -a x86 -l 26 ")
+	parser.add_argument( '-A',"--alfa_64"  , metavar='' , action =None,help ="generate AAAAAAAAtoZZZZZZZZ 64bit ""Exsample :-A x64 -l 26")
+	parser.add_argument( '-H',"--Hexdecode", metavar='' , action =None ,help ="to decode hex address ""Exampel: -H 0x00000000", type =str)
+	parser.add_argument( '-l',"--length"   , metavar='' , action =None,help ="to give length of the pattern ", type = int)
+	parser.add_argument( '-s',"--L_endian", metavar='' , action =None ,help ="to little-endian struct " "Exsampel: -s 0x0876bf67") 
+	self.args = parser.parse_args()
+	self.Main()
+	
     def ALFA__(self):
         try: 
             argv = sys.argv[2]  
@@ -41,10 +42,12 @@ class Tools :
                  len_long_1 = self.args.length                                     
                  result = "".join(random.choice(string.ascii_letters)for i in range(len_long_1)).lower() 
                  print result 
+                 with open("./.data",'w')as data:
+                    self.dataW = data.write(result)
                  exit()
             else:
                  print"Usege : [Option] -r [arguments] r [OPtion] -l [arguments] Number of length."
-                 print" exsampel: -r  r -l 100" 
+                 print"Exsampel: -r  r -l 100" 
                  exit()
     def AAAA(self): 
         try:    
@@ -55,12 +58,15 @@ class Tools :
                  ALfa = ''.join((string.ascii_uppercase[i:i+1]*4)for i in range(len_alfa)) 
                  print ALfa
                  exit()
+                 with open("./.data",'w')as data:
+                    self.dataW =  data.write(ALfa)
+                 exit()    
             else:
                  print"Usege : [Option] -a [arguments] x86 [OPtion] -l [arguments] Number of length."
                  print" exsampel: -a  x86 -l 26"            
         except UnboundLocalError:
                  print"Usege : [Option] -a[arguments] x86 [OPtion] -l [arguments] Number of length."
-                 print" exsampel: -a  x86 -l 26"      
+                 print"Exsampel: -a  x86 -l 26"      
                             
     def x86_64(self):
         try:    
@@ -70,10 +76,12 @@ class Tools :
             if len_alfa_1 == 1 or len_alfa_1 < 27:                    
                  ALfa_1 = ''.join((string.ascii_uppercase[i:i+1]*8)for i in range(len_alfa_1)) 
                  print ALfa_1
+                 with open("./.data",'w')as data:
+                     self.dataW = data.write(ALfa_1)
                  exit()
             else:
-                 print"Usege : [Option] -r [arguments] x64 [OPtion] -l [arguments] Number of length."
-                 print" exsampel: -A  x64 -l 26"            
+                 print"Usege : [Option] -A [arguments] x64 [OPtion] -l [arguments] Number of length."
+                 print"Exsampel: -A  x64 -l 26"            
         except UnboundLocalError:
                  print"Usege : [Option] -A [arguments] x64 [OPtion] -l [arguments] Number of length."
                  print"Exsampel: -A  x64 -l 26"                           
@@ -85,11 +93,24 @@ class Tools :
                  hexdecode = hexdecode.replace("0x","")
                  hexdecode = hexdecode.decode("hex")
                  print"decode HEX is : ", hexdecode
+                 with open("./.data",'r')as data:
+                     find = data.read()                    
+                 located = find.find(hexdecode)
+                 if hexdecode in find  :
+                    print "Offset Found at  : ",located      
+                    if os.path.isfile("./.data"):
+                       os.remove("./.data")
+                    else:
+                        pass                        
+                 else:
+                       print "No Offset Found "
             else:
                  print"Usege : [Option] -H [arguments]  HexNumber "
-                 print"Exsampel: -H 0x00000000 "                                 
+                 print"Exsampel: -H 0x00000000 "
+        except IOError:
+                pass                                            
         except Exception :
-            print "[IndexError] "
+            print "[IndexError]-[TypeError]"
             exit()               
             
     def little_endian1(self):
@@ -99,7 +120,7 @@ class Tools :
                little_endian  =   little_endian.replace("0x","")
                little_endian =   little_endian[::-1]
                little_endian = "".join('\\x%s'% little_endian[i:i+2] for i in range(0, len( little_endian), 2))
-               little_endian = "".join('"\\x%s"'% little_endian[i:i+16] for i in range(0, len( little_endian), 16))
+               little_endian = "".join('"\\x%s"'% little_endian[i:i+32] for i in range(0, len( little_endian), 32))
                little_endian.replace(" ","")
                little_endian.replace(" ","")
                little_endian= little_endian.replace('"\\x','"')               
@@ -127,10 +148,10 @@ class Tools :
                     
         if self.args.L_endian:
              self.little_endian1()
-             
-                     
+                                  
 if __name__=="__main__":
     Tools()
+
 
 
 
